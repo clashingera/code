f2aeb692263c67bb34b70e00b0063b57a9902ffe
clashingera <142937451+clashingera@users.noreply.github.com>
2024-05-11 08:28:42 +0530

    Update IS1_HEllo.py

--- IS1_HEllo.py
+++ IS1_HEllo.py
@@ -1,12 +1,24 @@
-str="Hello World"
+# Define a string variable
+str = "Hello World"
+
+# Iterate through each character in the string
 for x in str:
+    # Print the current character
     print(x)
-    print("Position of each character in string:",str.index(x))
-xoroperator=127^str.index(x)
+    # Print the position of the current character in the string
+    print("Position of each character in string:", str.index(x))
+
+# Calculate XOR operator between 127 and the index of the last character in the string
+xoroperator = 127 ^ str.index(x)
+# Print the result of XOR operator
 print("Print the result of XOR operator:", xoroperator)
 
-andoperator=127&str.index(x)
+# Calculate AND operator between 127 and the index of the last character in the string
+andoperator = 127 & str.index(x)
+# Print the result of AND operator
 print("Print the result of AND operator:", andoperator)
 
-oroperator=127|str.index(x)
+# Calculate OR operator between 127 and the index of the last character in the string
+oroperator = 127 | str.index(x)
+# Print the result of OR operator
 print("Print the result of OR operator:", oroperator)
